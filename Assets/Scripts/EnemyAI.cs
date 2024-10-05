9007af2d58700837cb429ed42eb90b22b5963203
Reichii-Dev <reyzramadhan2005@gmail.com>
2024-10-05 21:33:53 +0700

    Penambahan AI (Chasing)

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public GameObject player;
+    public float distanceBetween;
+
+    public float speed;
+    private float distance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,12 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed *Time.deltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
     }
-}
+
